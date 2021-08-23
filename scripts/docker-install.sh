f02d67c478939e665a7928a38366d4131deb960c
$ <$>
2021-08-23 09:33:54 +0700

    Add geonode

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,2 +1,5 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
